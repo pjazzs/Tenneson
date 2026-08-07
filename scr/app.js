5af11cd4b66e191a85d98ea344c7357997ba84ed
Opeyemi6794 <opeyemishittu72@gmail.com>
2026-08-07 15:24:14 +0100

    update cors

--- scr/app.js
+++ scr/app.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-frontend-url.vercel.app",
+      "https://tenneson.vercel.app/",
     ],
     credentials:true,
   })
