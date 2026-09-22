773df89b21da556a6abadc40f5fb03b70490f8ed
Progress <chisomprogress17@gmail.com>
2026-09-22 11:52:52 +0100

    qrcode verification

--- scr/utils/generateQRCode.js
+++ scr/utils/generateQRCode.js
@@ -5,9 +5,13 @@ const generateQRCode = async (studentIdentifier) => {
     throw new Error("Student identifier is required.");
   }
 
+  console.log("QR FRONTEND_URL:", process.env.FRONTEND_URL);
+
   const encodedIdentifier = encodeURIComponent(studentIdentifier.toString());
 
   const verificationUrl = `${process.env.FRONTEND_URL}/verify/${encodedIdentifier}`;
+
+  console.log("QR verification URL:", verificationUrl);
 
   return await QRCode.toDataURL(verificationUrl);
 };
